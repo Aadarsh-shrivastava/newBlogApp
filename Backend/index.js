bea3e9b4699e32dbd00cca7bbbc402a9ac6d213d
arpit910 <shrivastavaarpit910@gmail.com>
2024-03-07 17:02:37 +0530

    hello world

--- Backend/index.js
+++ Backend/index.js
@@ -1,31 +1,28 @@
-const express=require('express')
-const app=express()
-require('dotenv').config();
-const cors=require("cors")
+const express = require("express");
+const app = express();
+require("dotenv").config();
+const cors = require("cors");
 
-const userRoutes=require('./routes/user.routes.js')
-const authRoutes=require('./routes/auth.routes.js')
-const messageRoute =require('./routes/message.routes.js')
+const userRoutes = require("./routes/user.routes.js");
+const authRoutes = require("./routes/auth.routes.js");
+const messageRoute = require("./routes/message.routes.js");
 
-
-const connection=require('./database.js')
+const connection = require("./database.js");
 //Connecting Database
-connection()
-
+connection();
 
 //middleware.
 app.use(express.json());
 app.use(cors());
+app.use("/", (req, res) => {
+  res.send("hello world");
+});
 
+app.use("/api/users", userRoutes);
+app.use("/api/auth", authRoutes);
+app.use("/message", messageRoute);
 
-app.use("/api/users",userRoutes);
-app.use("/api/auth",authRoutes)    
-app.use("/message",messageRoute)
-
-
-
-const PORT =process.env.PORT||5000;
-app.listen(PORT,(req,res)=>{
-    console.log(`Server is running on port:${PORT}`);
-})
-
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, (req, res) => {
+  console.log(`Server is running on port:${PORT}`);
+});
